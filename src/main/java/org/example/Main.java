12c322ef326558108732b908628a49905ea8fd35
Yana_Ratnikova <yardley1@yandex.ru>
2023-12-07 14:31:49 +0300

    Ввод данных и проверка введенных данных

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,34 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите данные (Фамилия Имя Отчество дата_рождения номер_телефона пол): ");
+        String input = scanner.nextLine();
+        String[] data = input.split(" ");
+        if (data.length != 6){
+            System.out.println("Ошибка: введено неверное количество параметров");
+            return;
+        }
+        String LastName = data[0];
+        String firstName = data[1];
+        String patronymic = data[2];
+        String birthDate = data[3];
+        String phoneNumber = data[4];
+        String gender = data[5];
+        if (!isCorrectBirthDate(birthDate)){
+            System.out.println("Ошибка: неверный формат даты рождения. Ожидается дата в формате дд.мм.гг");
+            return;
+        }
+        if (!isCorrectPhoneNumber(phoneNumber)){
+            System.out.println("Ошибка: неверный формат номера телефона. Ожидается номер в формате +7ХХХХХХХХХХ");
+            return;
+        }
+        if (!isCorrectGender(gender)){
+            System.out.println("Ошибка: неверное значение пола. Ожидается m или f");
+            return;
+        }
     }
 }
